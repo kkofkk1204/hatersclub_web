b897afa7a6bcb5248897b4ac7371a5f7f9ace25d
kkofkk1204 <101874345+kkofkk1204@users.noreply.github.com>
2022-05-30 20:29:57 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -142,16 +142,19 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     if (data.totalSupply < 2000) {totalCostWei = 0};
+
+    console.log(blockchain.smartContract.methods.starttimestamp());
     blockchain.smartContract.methods
-      .starttimestamp(() => {
-        // console.log();
-        // if (err == null) {totalCostWei = 50000000000000000;}
-        // else if (Date.now() > err && nowtime < err + 1800) {totalCostWei = 50000000000000000;}
-        // else if (Date.now() > err + 1800 && nowtime < err + 3600) {totalCostWei = 40000000000000000;}
-        // else if (Date.now() > err + 3600 && nowtime < err + 5400) {totalCostWei = 30000000000000000;}
-        // else if (Date.now() > err + 5400 && nowtime < err + 7200) {totalCostWei = 20000000000000000;}
-        // else if (Date.now() > err + 7200) {totalCostWei = 10000000000000000;}
-      })
+      // .starttimestamp(() => {
+      //   // console.log();
+      //   // if (err == null) {totalCostWei = 50000000000000000;}
+      //   // else if (Date.now() > err && nowtime < err + 1800) {totalCostWei = 50000000000000000;}
+      //   // else if (Date.now() > err + 1800 && nowtime < err + 3600) {totalCostWei = 40000000000000000;}
+      //   // else if (Date.now() > err + 3600 && nowtime < err + 5400) {totalCostWei = 30000000000000000;}
+      //   // else if (Date.now() > err + 5400 && nowtime < err + 7200) {totalCostWei = 20000000000000000;}
+      //   // else if (Date.now() > err + 7200) {totalCostWei = 10000000000000000;}
+      // })
+      
       .mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
